1d14cb8c4772dd49bc913042f9d23c426c804209
mok2ee <tkekfrns@naver.com>
2022-08-22 11:42:40 +0900

    Edit UserSignUpForm.java

--- src/main/java/com/clonecoding/spring/naver/controller/UserSignUpForm.java
+++ src/main/java/com/clonecoding/spring/naver/controller/UserSignUpForm.java
@@ -1,5 +1,97 @@
 package com.clonecoding.spring.naver.controller;
 
-public class UserSignUpForm {
+import java.util.Date;
 
+public class UserSignUpForm {
+    private String userId;
+    private String userPw;
+    private String re;
+    private String name;
+    private Date birth;
+    private Boolean gender;
+    private String email;
+
+    private String year;
+    private String month;
+    private String day;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserPw() {
+        return userPw;
+    }
+
+    public void setUserPw(String userPw) {
+        this.userPw = userPw;
+    }
+
+    public String getRe() {
+        return re;
+    }
+
+    public void setRe(String re) {
+        this.re = re;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Boolean getGender() {
+        return gender;
+    }
+
+    public void setGender(Boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
 }
